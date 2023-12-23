869445d1b09e51e5eddf3fff7bb7a3247a8b6fa7
Anton Melnychuk <78281795+anton-mel@users.noreply.github.com>
2023-12-22 21:53:05 -0500

    update

--- index.js
+++ index.js
@@ -50,12 +50,12 @@ app.use(session({
 	store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI}),
 }));
 
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:8081');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    next();
-});
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', 'http://localhost:8081');
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+//     next();
+// });
 
 // Middleware
 app.use((req, res, next) => {

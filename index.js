6b01acfc5fa6cc26ef4302731a117ebe16d8e002
Anton Melnychuk <78281795+anton-mel@users.noreply.github.com>
2023-12-22 21:28:03 -0500

    Index

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ const events = require("./routes/events");
 const subscribe = require("./routes/subscribe");
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, { useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 mongoose.connection.on('connected', () => {
 	console.log('MongoDB connected successfully');

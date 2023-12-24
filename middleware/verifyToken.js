457e50c412c349934900f09344620c1105758631
Anton Melnychuk <78281795+anton-mel@users.noreply.github.com>
2023-12-24 17:38:03 -0500

    Handle Unauthorized

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -2,20 +2,26 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = async (req, res, next) => {
-    const authorizationHeader = req.headers['authorization'];
+  const authorizationHeader = req.headers['authorization'];
 
-    if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
-        return res.status(401).json({ message: 'Unauthorized' });
+  if (!authorizationHeader || !authorizationHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  const token = authorizationHeader.split(' ')[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.userId;
+    return next();
+  } catch (error) {
+    console.error('Error verifying token:', error);
+
+    // Redirect to login page on localhost:8081
+    if (req.headers.host === 'localhost:8081') {
+      return res.redirect('http://localhost:8081/login');
     }
 
-    const token = authorizationHeader.split(' ')[1];
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.userId = decoded.userId;
-        return next();
-    } catch (error) {
-        console.error('Error verifying token:', error);
-        return res.status(403).json({ message: 'Invalid Token' });
-    }
-};
+    return res.status(403).json({ message: 'Invalid Token' });
+  }
+}

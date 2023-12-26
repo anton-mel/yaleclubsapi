6d7597106bcd6a998edf30d1d42c179b03f31891
Anton Melnychuk <78281795+anton-mel@users.noreply.github.com>
2023-12-25 20:12:27 -0500

    delete

--- routes/delete.js
+++ routes/delete.js
@@ -1,21 +1,17 @@
 const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middleware/verifyToken');
-const User = require('../models/user');
 
 router.post('/delete-club', verifyToken, async (req, res) => {
   try {
     const { clubId } = req.body;
-    const userId = req.userId;
-    
-    const user = await User.findOne({ userId });
+    const user = req.user;
     const ClubIndex = user.saved.indexOf(clubId);
     
     if (ClubIndex === -1) {
       return res.status(400).json({ error: 'Club ID not found in user\'s saved clubs' });
     }
     
-    res.status(200).json({ ClubIndex });
     user.saved.splice(ClubIndex, 1);
     await user.save();
 

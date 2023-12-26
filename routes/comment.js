15b34a0df13bca6cea2520085d3b3242e1d724f5
Anton Melnychuk <78281795+anton-mel@users.noreply.github.com>
2023-12-25 20:47:56 -0500

    Comment Update

--- routes/comment.js
+++ routes/comment.js
@@ -1,7 +1,7 @@
-require('dotenv').config();
-var router = express.Router();
-const axios = require("axios");
 var express = require("express");
+var router = express.Router();
+require('dotenv').config();
+const axios = require("axios");
 const Comment = require('../models/comment');
 const verifyToken = require("../middleware/verifyToken");
 

7cfff3bc69099d5de85ab7bb5c9c6f3dde1fdb74
Anton Melnychuk <78281795+anton-mel@users.noreply.github.com>
2023-12-26 18:12:13 -0500

    auth

--- routes/auth.js
+++ routes/auth.js
@@ -46,7 +46,7 @@ router.get('/auth/redirect', async (req, res) => {
         }
     
         // Return the Token
-        res.redirect(`yaleclubs://?token=${token}`);
+        res.redirect(`http://localhost:8081/login?token=${token}`);
     } catch (error) {
         console.error('Error in CAS redirection:', error);
         res.status(500).send('Internal Server Error');
